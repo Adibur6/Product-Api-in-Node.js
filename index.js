ed42a8601cfcb7bec12544084295759f1db1ccdf
adibur6 <adibur6@gmail.com>
2023-10-25 18:37:18 +0600

    Products logic done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000; // You can choose any port you like
 
-mongoose.connect('mongodb://127.0.0.1:27017', {
+mongoose.connect("mongodb://127.0.0.1:27017/Products", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -19,47 +19,80 @@ const productSchema = new mongoose.Schema({
   availableAmount: Number,
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 app.use(bodyParser.json());
 
 // Set up your API routes
-app.get('/products', async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.post('/products', async (req, res) => {
+app.post("/products", async (req, res) => {
   try {
-    const newProduct = new Product(req.body);
+    const { name, id, price, unit, availableAmount } = req.body;
+
+    // Create a new product instance and validate it
+    const newProduct = new Product({ name, id, price, unit, availableAmount });
+    const { error } = newProduct.validateSync();
+
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
     await newProduct.save();
     res.json(newProduct);
   } catch (error) {
-    res.status(400).json({ error: 'Invalid request data' });
+    res.status(400).json({ error: "Invalid request data" });
   }
 });
 
-app.put('/products/:id', async (req, res) => {
+app.put("/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
-    const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, { new: true });
+
+    // Validate the request data against the schema
+    const { name, price, unit, availableAmount } = req.body;
+    const productData = { name, price, unit, availableAmount };
+    const { error } = productSchema.validate(productData);
+
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const updatedProduct = await Product.findOneAndUpdate(
+      { id: productId },
+      productData,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
     res.json(updatedProduct);
   } catch (error) {
-    res.status(400).json({ error: 'Invalid request data' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.delete('/products/:id', async (req, res) => {
+app.delete("/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
-    await Product.findByIdAndRemove(productId);
-    res.json({ message: 'Product deleted' });
+    const product = await Product.findOneAndRemove({ id: productId });
+
+    if (!product) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.json({ message: "Product deleted" });
+    }
   } catch (error) {
-    res.status(400).json({ error: 'Invalid request data' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
